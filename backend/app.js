0ec773cba7e12f79ba65e501a95486021cb29f68
Fred Boniou <f.boniou@gmail.com>
2020-09-04 14:45:55 +0200

    ajout mongoose et connexion mongoDB

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,15 @@ const express = require("express"); //import du package express
 
 const app = express(); //création de l'appli express
 
-const bodyParser = require("body-parser"); //Import package body-parser pour gérer la demande POST provenant de l'appli front end
+const bodyParser = require("body-parser"); //import package body-parser pour gérer la demande POST provenant de l'appli front end
+
+const mongoose = require("mongoose"); // import du package mongoose pour les interactions avec mongoDB
+
+mongoose.connect('mongodb+srv://FredBoniou:Myriam26121983.@cluster0.vqduv.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req,res,next) => {
     res.setHeader("Access-Control-Allow-Origin", "*"); //Accès à l'API depuis n'importe quelle origine
@@ -14,7 +22,7 @@ app.use((req,res,next) => {
 app.use(bodyParser.json()); //méthode JSON pour transformer la requête en objet JS utilisable
 
 app.use((req, res) => {
-    res.json({ message: "et aller, go pour le p6 !!!!" });
+    res.json({ message: "et aller, go pour le p6 !!!! on lache rien !!!" });
 });
 
 module.exports = app; //export appli pour y accéder dans les autres fichiers, notamment server.js 
