391b2b62612b559bb9cf674bcf038e2c22188d52
Fred Boniou <f.boniou@gmail.com>
2020-09-04 11:43:11 +0200

    ajout des headers dans app.js

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,13 @@ const express = require("express"); //import du package express
 
 const app = express(); //création de l'appli express
 
+app.use((req,res,next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*"); //Accès à l'API depuis n'importe quelle origine
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"); //Ajout des Headers aux requêtes API
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS"); //Envoi des requêtes avec les methodes mentionnées
+    next();
+});
+
 app.use((req, res) => {
     res.json({ message: "et aller, go pour le p6 !!!!" });
 });
