d4164751c13844d9bbf5635b62a1b46704c5e975
Fred Boniou <f.boniou@gmail.com>
2020-09-04 15:33:52 +0200

    création de sauces.js

---
+++ backend/models/sauces.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose"); //import mongoose
+
+const sauceSchema = mongoose.Schema({ //méthode Schema de mongoose pour créer un schéma de données qui contient les champs souhaités pour chaque sauces
+    userId = { type: String, required: true },
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    description: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    heat: { type: Number, required: true },
+    likes: { type: Number },
+    dislikes: { type: Number },
+    usersLiked: { type: [String] },
+    usersDisliked: { type: [String] },
+});
+
+module.exports = mongoose.model("Sauce", sauceSchema); //export du schéma en tant que modèle mongoose pour disponibilité pour appli express
+
