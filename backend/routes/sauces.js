339a589b13d6ca12323b2a8b741e9d204082d741
Fred Boniou <f.boniou@gmail.com>
2020-09-07 17:43:34 +0200

    middleware modifications sauces

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -2,7 +2,7 @@ const express = require("express"); //import package express
 
 const router = express.Router(); //création d'un routeur express
 
-const Sauce = require("../models/sauces");
+const Sauce = require("../models/sauce");
 
 router.get("/", (req, res, next) => { //afficher toutes les sauces
     Sauce.find()
@@ -13,7 +13,7 @@ router.get("/", (req, res, next) => { //afficher toutes les sauces
 router.get("/:id", (req, res, next) => { //affichage d'une sauce en fonction de l'id
     Sauce.findOne({_id: req.params.id})
         .then((sauce) => res.status(200).json(sauce))
-        .catch(error => res.status(400).json({error}));
+        .catch(error => res.status(404).json({error})); //erreur 404 pour un objet introuvable
 });
 
 router.post("/", (req, res, next) => { //création et enregistrement d'une sauce
@@ -21,7 +21,13 @@ router.post("/", (req, res, next) => { //création et enregistrement d'une sauce
         ...req.body
     });
     sauce.save()
-        .then(() => res.status(200).json({message: "Sauce créée !"}))
+        .then(() => res.status(201).json({message: "Sauce créée !"})) //201 pour une création réussie
+        .catch(error => res.status(400).json({error}));
+});
+
+router.put("/:id", (req, res, next) => { // mise à jour/modification de la sauce avec l'id
+    Sauce.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
+        .then(() => res.status(200).json({message: "Sauce modifiée !"}))
         .catch(error => res.status(400).json({error}));
 });
 
