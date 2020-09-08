ed295bb5a97ee655caff036b5324b9d2061dbeab
Fred Boniou <f.boniou@gmail.com>
2020-09-08 18:16:42 +0200

    implémentation token authentification

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -4,15 +4,17 @@ const router = express.Router(); //création d'un routeur express
 
 const sauceCtrl = require("../controllers/sauces"); //import sauceCtrl
 
-router.get("/", sauceCtrl.getAllSauces); //afficher toutes les sauces
+const auth = require("../middleware/auth"); // import middleware authentification
 
-router.get("/:id", sauceCtrl.getOneSauce); //affichage d'une sauce en fonction de l'id
+router.get("/", auth, sauceCtrl.getAllSauces); //afficher toutes les sauces
 
-router.post("/", sauceCtrl.createSauce); //création et enregistrement d'une sauce
+router.get("/:id", auth, sauceCtrl.getOneSauce); //affichage d'une sauce en fonction de l'id
 
-router.put("/:id", sauceCtrl.modifySauce); // mise à jour/modification de la sauce avec l'id
+router.post("/", auth, sauceCtrl.createSauce); //création et enregistrement d'une sauce
 
-router.delete("/:id", sauceCtrl.deleteSauce); //suppression d'une sauce
+router.put("/:id", auth, sauceCtrl.modifySauce); // mise à jour/modification de la sauce avec l'id
+
+router.delete("/:id", auth, sauceCtrl.deleteSauce); //suppression d'une sauce
 
 module.exports = router;
 
